3868182f45307925f24864827cbae0418acb4f35
CNLHC <cn_lhc@qq.com>
2019-11-13 18:57:41 +0800

    universe layoout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,65 +1,39 @@
 import React, { useState } from 'react';
-import { Layout, Menu, Icon } from 'antd';
-const { Header, Content, Sider } = Layout;
-const { SubMenu } = Menu;
+import { Layout } from 'antd';
+import axios from 'axios';
+import SideBar from './SideBar';
+import TopBar from './TopBar';
+const { Content } = Layout;
 
-export default function MainLayout(props: {
+function MainLayout(props: {
   children?: React.ReactNode[] | React.ReactNode | React.ReactChild;
 }) {
   const [collapse, setCollapse] = useState(false);
-
   return (
     <Layout style={{ minHeight: '100vh' }} hasSider>
-      <Sider
-        collapsible
-        collapsed={collapse}
-        onCollapse={() => setCollapse(e => !e)}
-      >
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1">
-            <Icon type="pie-chart" />
-            <span>Option 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <Icon type="user" />
-                <span>User</span>
-              </span>
-            }
-          >
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="team" />
-                <span>Team</span>
-              </span>
-            }
-          >
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9">
-            <Icon type="file" />
-            <span>File</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
+      <SideBar collapse={collapse} onCollapse={() => setCollapse(e => !e)} />
       <Layout>
-        <Header style={{ background: '#fff', padding: 0 }} />
-        <Content>{props.children}</Content>
+        <TopBar />
+        <Content
+          style={{
+            margin: '1em',
+            background: '#FFF',
+            padding: '1em',
+          }}
+        >
+          {props.children}
+        </Content>
       </Layout>
     </Layout>
   );
 }
+
+MainLayout.getInitialProps = async function() {
+  const res = await axios.post('/_auth/getToken', {
+    username: 'foo',
+    password: 'bar',
+  });
+  console.log(res.data);
+};
+
+export default MainLayout;
