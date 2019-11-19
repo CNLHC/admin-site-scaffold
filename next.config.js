3d17df44f4af8611d2bc140270fc12b0d22708d4
CNLHC <cn_lhc@qq.com>
2019-11-19 20:11:46 +0800

    Tweak for jacinth build

---
+++ next.config.js
@@ -0,0 +1,1 @@
+module.exports = require('./node_modules/@jacinthjs/jacinth-dev/dist/wrapper/next.config');
---
+++ src/libs/API/get_benchmark.ts
@@ -0,0 +1,10 @@
+
+
+export interface Request {
+    taskid:  any[];
+    count:   number;
+    offset:  number;
+    product: string[];
+    version: string[];
+    video:   string[];
+}
